7ee35049a84847334adefaa30c7003cf996ef107
Alexander Halim <akhalim@ucsc.edu>
2024-03-03 18:24:19 -0800

    adding filling heart function + text indicator

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -6,6 +6,7 @@ class PlayScene extends BaseScene {
     this.sentenceProgress = 0;
     this.currentRound = null;
     this.currentRoundIndex = 0;
+    this.sentenceText = null;
   }
 
   init() {
@@ -25,6 +26,7 @@ class PlayScene extends BaseScene {
     this.load.image("background", "assets/background.png");
     this.load.image("clicker", "assets/clicker.png");
     this.load.image("heart", "assets/unfilledheart.png");
+    this.load.image("filledHeart", "assets/filledheart.png");
   }
 
   create() {
@@ -34,7 +36,7 @@ class PlayScene extends BaseScene {
       .setOrigin(0, 0);
 
     for (let i = 0; i < this.maxHearts; i++) {
-      let heart = this.add.sprite(100 + i * 80, 30, "heart").setScale(0.5);
+      let heart = this.add.sprite(100 + i * 80, 30, "heart").setScale(0.25);
       this.hearts.push(heart);
     }
 
@@ -71,14 +73,14 @@ class PlayScene extends BaseScene {
 
     this.rounds = [
       new Round(
-        "first sentence",
+        "This is an example sentence",
         30000,
         this.completeRound.bind(this),
         this.failRound.bind(this)
       ),
 
       new Round(
-        "second sentence",
+        "This is the second sentence",
         30000,
         this.completeRound.bind(this),
         this.failRound.bind(this)
@@ -133,13 +135,12 @@ class PlayScene extends BaseScene {
       if (event.key === currentSentence[this.sentenceProgress]) {
         this.sentenceProgress++;
         this.tickPuppy();
+        this.updateTextProgress();
 
         // If the entire sentence has been correctly typed
         if (this.sentenceProgress >= currentSentence.length) {
           console.log("Sentence completed.");
           this.completeRound();
-          // **Reset for the next sentence
-          this.sentenceProgress = 0;
         }
       } else {
         this.loseHeart();
@@ -149,12 +150,14 @@ class PlayScene extends BaseScene {
   startNextRound() {
     if (this.currentRoundIndex < this.rounds.length) {
       console.log("Round starting.");
+
       this.currentRound = this.rounds[this.currentRoundIndex];
       this.currentRound.start();
-      this.sentence = this.currentRound.sentence;
+      this.displaySentence(this.currentRound.sentence);
       this.currentRoundIndex++;
     } else {
       console.log("All rounds finished.");
+      this.sentenceText.setVisible(false);
     }
   }
   completeRound() {
@@ -167,8 +170,48 @@ class PlayScene extends BaseScene {
     console.log("Failed round.");
   }
 
+  displaySentence(sentence) {
+    if (!this.sentenceText) {
+      this.sentenceText = this.add
+        .text(this.config.width / 2, 50, sentence, {
+          font: "18px Arial",
+          fill: "#ffffff",
+          align: "center",
+        })
+        .setOrigin(0.5);
+    } else {
+      this.sentenceText.setText(sentence);
+    }
+    this.sentenceText.setVisible(true);
+    // Reset progress here
+    this.sentenceProgress = 0;
+  }
+  updateTextProgress() {
+    // Only the remaining part of the sentence should be displayed.
+    let remainingPart = this.currentRound.sentence.substring(
+      this.sentenceProgress
+    );
+
+    // Update the displayed sentence to only show the remaining part.
+    this.sentenceText.setText(remainingPart);
+  }
   // todo
   fillHeart() {
     console.log("Filling heart!");
+
+    if (this.hearts.length > 0) {
+      // Find the first unfilled heart
+      const unfilledHeartIndex = this.hearts.findIndex(
+        (heart) => heart.texture.key === "heart"
+      );
+      if (unfilledHeartIndex !== -1) {
+        const unfilledHeart = this.hearts[unfilledHeartIndex];
+
+        // Set the texture to filled heart
+        unfilledHeart.setTexture("filledHeart");
+
+        console.log("Heart filled!");
+      }
+    }
   }
 }
